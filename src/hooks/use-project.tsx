03ac66a49bdc2a93a9458f2a40bd22e4baa60f54
Asaiy420 <kcaryan05@gmail.com>
2025-08-09 21:01:29 +0545

    feat: add useProject and useRefetch hooks for project management and query refetching

---
+++ src/hooks/use-project.tsx
@@ -0,0 +1,18 @@
+import { api } from "@/trpc/react";
+import React from "react";
+import { useLocalStorage } from "usehooks-ts";
+
+const useProject = () => {
+  const { data: projects } = api.project.getProjects.useQuery();
+  const [projectId, setProjectId] = useLocalStorage("blaze-project", "");
+  const project = projects?.find((project) => projectId === projectId);
+
+  return {
+    projects,
+    project,
+    projectId,
+    setProjectId
+  };
+};
+
+export default useProject;
