c21d2275bad7b7ee206a25bca11e44f1a5aebfd5
Asaiy420 <kcaryan05@gmail.com>
2025-08-10 11:25:41 +0545

    feat: enhance DashboardPage with project GitHub link display

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,15 +1,50 @@
 "use client";
 
-import { useUser } from '@clerk/nextjs';
-import React from 'react'
+import useProject from "@/hooks/use-project";
+import { useUser } from "@clerk/nextjs";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const DashboardPage = () => {
-    const {user} = useUser();
+  const { project } = useProject();
   return (
     <div>
-        {user?.firstName}
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        {/* GITHUB LINK */}
+        <div className="bg-primary w-fit rounded-md px-4 py-3">
+          <div className="flex items-center">
+            <Github className="size-5 text-white" />
+            <div className="ml-2">
+              <p className="text-sm font-medium text-white">
+                This project is linked to{" "}
+                <Link
+                  href={project?.githubUrl ?? ""}
+                  className="inline-flex items-center text-white/80 hover:underline"
+                >
+                  {project?.githubUrl}
+                  <ExternalLink className="ml-1 size-4" />
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="h-4"></div>
+        <div className="flex items-center gap-4">
+          Team Members Invite Button Archive
+        </div>
+        <div>
+          <div mt-4>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+              Ask Question Card Meeting Card
+            </div>
+          </div>
+          <div className="mt-8"></div>
+          Commit log
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
