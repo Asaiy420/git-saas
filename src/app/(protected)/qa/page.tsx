b30cc1ce52caf48744cf9b7852851316bf7e21b3
Asaiy420 <kcaryan05@gmail.com>
2025-08-14 18:20:37 +0545

    feat: implement QaPage component with question retrieval and display functionality

--- src/app/(protected)/qa/page.tsx
+++ src/app/(protected)/qa/page.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
+'use client'
+
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import useProject from "@/hooks/use-project";
+import { api } from "@/trpc/react";
+import React, { Fragment, useState } from "react";
+import AskQuestionCard from "../dashboard/ask-question-card";
+import MDEditor from "@uiw/react-md-editor";
+import CodeReferneces from "../dashboard/code-references";
 
 const QaPage = () => {
+  const { projectId } = useProject();
+  const { data: questions } = api.project.getQuestion.useQuery({ projectId });
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const question = questions?.[questionIndex];
   return (
-    <div>QaPage</div>
-  )
-}
+    <Sheet>
+      <AskQuestionCard />
+      <div className="h-4"></div>
+      <h1 className="text-sm font-semibold">Saved Questions</h1>
+      <div className="h-2"></div>
+      <div className="flex flex-col gap-2">
+        {questions?.map((question, idx) => {
+          return (
+            <Fragment key={question.id}>
+              <SheetTrigger onClick={() => setQuestionIndex(idx)}>
+                <div className="flex items-center gap-4 rounded-lg border bg-white p-4 shadow">
+                  <img
+                    className="rounded-full"
+                    height={30}
+                    width={30}
+                    src={question.user.imageUrl ?? ""}
+                  />
+                  <div className="flex flex-col text-left">
+                    <div className="flex items-center gap-2">
+                      <p className="line-clamp-1 text-lg font-medium text-gray-700">
+                        {question.question}
+                      </p>
+                      <span className="text-xs whitespace-nowrap text-gray-400">
+                        {question.createdAt.toLocaleDateString()}
+                      </span>
+                    </div>
+                    <p className="line-clamp-1 text-sm text-gray-500">
+                      {question.answer}
+                    </p>
+                  </div>
+                </div>
+              </SheetTrigger>
+            </Fragment>
+          );
+        })}
+      </div>
+      {question && (
+        <SheetContent className="max-w-[80vw]">
+          <SheetHeader>
+            <SheetTitle>{question.question}</SheetTitle>
+            <MDEditor.Markdown source={question.answer} />
+            <CodeReferneces filesReferences={(question.filesReferences ?? []) as any}/>
+          </SheetHeader>
+        </SheetContent>
+      )}
+    </Sheet>
+  );
+};
 
-export default QaPage
+export default QaPage;
