5ee4d83357aebdd05cdd6c138fb87da672d535c8
Asaiy420 <kcaryan05@gmail.com>
2025-08-16 15:24:57 +0545

    feat: enhance MeetingCard to process uploaded meetings with AI analysis and improve upload flow

--- src/app/(protected)/dashboard/meeting-card.tsx
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -8,105 +8,121 @@ import { useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import { api } from "@/trpc/react";
+import axios from "axios";
 import useProject from "@/hooks/use-project";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
 
 const MeetingCard = () => {
-	const { project } = useProject();
-	const [isUploading, setIsUploading] = useState(false);
-	const [progress, setProgress] = useState(0);
-	const uploadMeeting = api.project.uploadMeeting.useMutation();
-	const router = useRouter();
-	const { getRootProps, getInputProps } = useDropzone({
-		accept: {
-			"audio/*": [".mp3", ".wav", ".m4a"],
-		},
-		multiple: false,
-		maxSize: 50_000_000,
-		onDrop: async (acceptedFiles) => {
-			if (!project) return;
-			setIsUploading(true);
-			const file = acceptedFiles[0];
-			if (!file) {
-				return "Please add a file first";
-			}
-			const downloadURL = (await uploadFile(
-				file as File,
-				setProgress,
-			)) as string;
-			uploadMeeting.mutate(
-				{
-					projectId: project.id,
-					meetingUrl: downloadURL,
-					name: file.name,
-				},
-				{
-					onSuccess: () => {
-						toast.success("Meeting uploaded successfully!");
-						router.push("/meetings");
-					},
-					onError: () => {
-						toast.error(
-							"Error when uploading the meeting. Please try again!",
-						);
-					},
-				},
-			);
-			setIsUploading(false);
-		},
-	});
+  const { project } = useProject();
 
-	return (
-		<Card
-			className="col-span-2 flex flex-col items-center justify-center border-none p-10 shadow-2xl"
-			{...getRootProps()}
-		>
-			{!isUploading && (
-				<>
-					<Presentation className="size-10 animate-bounce" />
-					<h3 className="mt-2 text-sm font-semibold text-gray-900">
-						Create a meeting
-					</h3>
-					<p className="mt-1 text-center text-sm text-gray-500">
-						Analyze your meeting with Blaze.
-						<br />
-						Powerd By AI
-					</p>
-					<div className="mt-10">
-						<Button
-							disabled={isUploading}
-							className="cursor-pointer"
-						>
-							<Upload
-								className="mr-1.5 -ml-0.5 size-5"
-								aria-hidden="true"
-							/>
-							Upload Meeting
-							<input className="hidden" {...getInputProps()} />
-						</Button>
-					</div>
-				</>
-			)}
+  const processMeeting = useMutation({
+    mutationFn: async (data: {
+      meetingUrl: string;
+      meetingId: string;
+      projectId: string;
+    }) => {
+      const { meetingUrl, meetingId, projectId } = data;
+      const response = await axios.post("/api/process-meeting", {
+        meetingUrl,
+        meetingId,
+        projectId,
+      });
+      return response.data;
+    },
+  });
 
-			{isUploading && (
-				<div>
-					<CircularProgressbar
-						value={progress}
-						text={`${progress}%`}
-						className="size-20"
-						styles={buildStyles({
-							pathColor: "#2563eb",
-							textColor: "#2563eb",
-						})}
-					/>
-					<p className="text-center text-sm text-gray-500">
-						Uploading your meeting...
-					</p>
-				</div>
-			)}
-		</Card>
-	);
+  const [isUploading, setIsUploading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const uploadMeeting = api.project.uploadMeeting.useMutation();
+  const router = useRouter();
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {
+      "audio/*": [".mp3", ".wav", ".m4a"],
+    },
+    multiple: false,
+    maxSize: 50_000_000,
+    onDrop: async (acceptedFiles) => {
+      if (!project) return;
+      setIsUploading(true);
+      const file = acceptedFiles[0];
+      if (!file) {
+        return "Please add a file first";
+      }
+      const downloadURL = (await uploadFile(
+        file as File,
+        setProgress,
+      )) as string;
+      uploadMeeting.mutate(
+        {
+          projectId: project.id,
+          meetingUrl: downloadURL,
+          name: file.name,
+        },
+        {
+          onSuccess: (meeting) => {
+            toast.success("Meeting uploaded successfully!");
+            router.push("/meetings");
+            processMeeting.mutateAsync({
+              meetingUrl: downloadURL,
+              meetingId: meeting.id,
+              projectId: project.id,
+            });
+          },
+          onError: () => {
+            toast.error("Error when uploading the meeting. Please try again!");
+          },
+        },
+      );
+      setIsUploading(false);
+    },
+  });
+
+  return (
+    <Card
+      className="col-span-2 flex flex-col items-center justify-center border-none p-10 shadow-2xl"
+      {...getRootProps()}
+    >
+      {!isUploading && (
+        <>
+          <Presentation className="size-10 animate-bounce" />
+          <h3 className="mt-2 text-sm font-semibold text-gray-900">
+            Create a meeting
+          </h3>
+          <p className="mt-1 text-center text-sm text-gray-500">
+            Analyze your meeting with Blaze.
+            <br />
+            Powerd By AI
+          </p>
+          <div className="mt-10">
+            <Button disabled={isUploading} className="cursor-pointer">
+              <Upload className="mr-1.5 -ml-0.5 size-5" aria-hidden="true" />
+              Upload Meeting
+              <input className="hidden" {...getInputProps()} />
+            </Button>
+          </div>
+        </>
+      )}
+
+      {isUploading && (
+        <div>
+          <CircularProgressbar
+            value={progress}
+            text={`${progress}%`}
+            className="size-20"
+            styles={buildStyles({
+              pathColor: "#2563eb",
+              textColor: "#2563eb",
+            })}
+          />
+          <p className="text-center text-sm text-gray-500">
+            Uploading your meeting...
+          </p>
+        </div>
+      )}
+    </Card>
+  );
 };
 
 export default MeetingCard;
