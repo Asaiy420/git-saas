411909ecbb3883048281c0dea67b1bca22c48e5d
Asaiy420 <kcaryan05@gmail.com>
2025-08-17 11:04:18 +0545

    feat: add InviteButton component for inviting team members

---
+++ src/app/(protected)/dashboard/invite-button.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import useProject from "@/hooks/use-project";
+import { useState } from "react";
+import { toast } from "sonner";
+
+const InviteButton = () => {
+  const { projectId } = useProject();
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite Team Members</DialogTitle>
+          </DialogHeader>
+          <p className="text-sm text-gray-500">
+            Ask your team members to copy and paste this link
+          </p>
+          <Input
+            className="mt-4"
+            readOnly
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `${window.location.origin}/join/${projectId}`,
+              );
+              toast.success("Copied to clipboard");
+            }}
+            value={`${window.location.origin}/join/${projectId}`}
+          />
+        </DialogContent>
+      </Dialog>
+      <Button
+        onClick={() => setOpen(true)}
+        size="sm"
+        className="cursor-pointer"
+      >
+        Invite Members
+      </Button>
+    </>
+  );
+};
+
+export default InviteButton;
